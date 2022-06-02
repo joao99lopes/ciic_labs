c5b3c72f0a623c26acba1b0fac3d5a858e52e386
lopesj <joao.lopes@studentlab.pt>
2022-06-02 15:28:15 +0100

    git angel

--- lab6-p1/lab6.py
+++ lab6-p1/lab6.py
@@ -29,80 +29,28 @@ target_bin = df['AboveLimit']
 data_temp, data_test, target_temp, target_test = train_test_split(data, target_bin, test_size = 0.20, random_state = 10)
 data_train, data_validation, target_train, target_validation = train_test_split(data_temp, target_temp, test_size = 0.25, random_state = 10)
 
-# lbfgs
-clf = MLPClassifier(activation='logistic', random_state=1, solver='lbfgs')
-pred = clf.fit(data_train,target_train).predict(data_validation)
-print("PROB lbfgs:",accuracy_score(target_validation,pred))
-visualizer = ClassificationReport(clf,support=True)
-visualizer.fit(data_train, target_train) # Fit the training data to the
-visualizer.score(data_validation, target_validation)
+clf = MLPClassifier(activation='logistic', alpha=1e-5, random_state=1, solver='lbfgs', hidden_layer_sizes=(4,2))
+pred = clf.fit(data_train.values,target_train.values).predict(data_validation.values)
+print("PROB BIN:",accuracy_score(target_validation,pred))
+visualizer = ClassificationReport(clf,classes=["Under limit","Above limit"])
+visualizer.fit(data_train.values, target_train.values)
+visualizer.score(data_validation.values, target_validation.values)
 g = visualizer.poof()
-
-
-# sgd
-#clf = MLPClassifier(activation='logistic', random_state=1, solver='sgd')
-#pred = clf.fit(data_train,target_train).predict(data_validation)
-#print("PROB sgd:",accuracy_score(target_validation,pred))
-#visualizer = ClassificationReport(clf)
-#visualizer.fit(data_train, target_train) # Fit the training data to the
-#visualizer.score(data_validation, target_validation)
-#g= visualizer.poof()
 
 
 #####################
 # Amount of Persons #
 #####################
 
-#data_temp, data_test, target_temp, target_test = train_test_split(data, target, test_size = 0.20, random_state = 10)
-#data_train, data_validation, target_train, target_validation = train_test_split(data_temp, target_temp, test_size = 0.25, random_state = 10)
+data_temp, data_test, target_temp, target_test = train_test_split(data, target, test_size = 0.20, random_state = 10)
+data_train, data_validation, target_train, target_validation = train_test_split(data_temp, target_temp, test_size = 0.25, random_state = 10)
 
-
-
-
-
-
-"""
-# LinearSVC
-from sklearn.svm import LinearSVC
-svc_model  = LinearSVC(random_state=0)
-pred = svc_model.fit(data_train,target_train).predict(data_validation)
-print("LinearSVC accuracy : ",accuracy_score(target_validation, pred,normalize = True))
-
-# Naive-Bayes
-from sklearn.naive_bayes import GaussianNB
-gnb = GaussianNB()
-pred = gnb.fit(data_train,target_train).predict(data_validation)
-print("Naive-Bayes accuracy : ",accuracy_score(target_validation, pred, normalize = True))
-
-# K-Neighbors Classifier
-from sklearn.neighbors import KNeighborsClassifier
-neigh = KNeighborsClassifier(n_neighbors=3)
-pred = neigh.fit(data_train,target_train).predict(data_validation)
-print ("KNeighbors accuracy score : ",accuracy_score(target_validation,pred))
-
-#####
-# Performance Comparison
-###
-from yellowbrick.classifier import ClassificationReport
-
-
-
-# GaussianNB
-visualizer = ClassificationReport(gnb,classes=["0 Persons","1 Persons","2 Persons","3 Persons"])
-visualizer.fit(data_train,target_train)
-visualizer.score(data_validation,target_validation)
+# lbfgs
+clf = MLPClassifier(activation='logistic', alpha=1e-5, random_state=1, solver='lbfgs', hidden_layer_sizes=(4,1), max_iter=256)
+pred = clf.fit(data_train.values,target_train.values).predict(data_validation.values)
+print("PROB lbfgs:",accuracy_score(target_validation,pred))
+visualizer = ClassificationReport(clf)
+visualizer.fit(data_train.values, target_train.values) # Fit the training data to the
+visualizer.score(data_validation.values, target_validation.values)
 g = visualizer.poof()
-
-# LinearSVC
-visualizer = ClassificationReport(svc_model,classes=["0 Persons","1 Persons","2 Persons","3 Persons"])
-visualizer.fit(data_train,target_train)
-visualizer.score(data_validation,target_validation)
-g = visualizer.poof()
-
-# KNeighborsClassifier
-visualizer = ClassificationReport(neigh,classes=["0 Persons","1 Persons","2 Persons","3 Persons"])
-visualizer.fit(data_train,target_train)
-visualizer.score(data_validation,target_validation)
-g = visualizer.poof()
-"""
 
